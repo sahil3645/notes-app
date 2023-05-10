b79529d60dd92a1130e8122d3cae1e769116800a
sahil3645 <sahilchaudhari.2311@gmail.com>
2023-05-10 20:45:00 +0530

    main commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NotesIcon from '@mui/icons-material/Notes';
+import DescriptionIcon from '@mui/icons-material/Description';
+
+function Header() {
+
+    return (
+        <header>
+            <h1><DescriptionIcon fontSize="medium" /> Keeper</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+    {
+        key: 1,
+        title: "this is one of the title",
+        content: "the content of notes go here"
+    },
+    {
+        key: 2,
+        title: "this is another title",
+        content: "the content of notes go here as well"
+    }
+];
+
+export default notes;
